11712b346bce950e3dd37f2bdd7594f3574bbed2
maratul <maratulf059@gmail.com>
2024-05-01 16:43:43 +0700

    Tugas-oop-php1

--- index.php
+++ index.php
@@ -3,4 +3,25 @@
 require_once "Frog.php";
 require_once "Ape.php";
 
+$sheep = new animal("Shaun");
+echo "Name : " .$sheep-> get_name();
+echo "<br/> Legs : " .$sheep-> get_legs();
+echo "<br/> Cold Blooded : " .$sheep-> get_cold_blooded();
+echo "<br/>";
+echo "<br/>";
+
+$kodok = new Frog("Buduk");
+$kodok->getFrogName();
+$kodok->getFrogLegs();
+$kodok->getFrogBlood();
+$kodok->getFrogJump();
+echo "<br/>";
+echo "<br/>";
+
+$ape = new Ape("Kera Sakti");
+$ape->getApeName();
+$ape->getApeLegs();
+$ape->getApeBlood();
+$ape->getApeYell();
+
 ?>
